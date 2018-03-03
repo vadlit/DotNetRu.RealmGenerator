9769c7be4a45da5213e1ae2be7a71d4e7d5a2074
Pavel <pavel.fedotovsky@outlook.com>
2018-03-04 00:24:50 +0300

    Added realm file generation

--- RealmGenerator/Program.cs
+++ RealmGenerator/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
-
-namespace RealmGenerator
+﻿namespace RealmGenerator
 {
-    class Program
+    using Realms;
+
+    public class Speaker : RealmObject
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new RealmConfiguration("C:\\Users\\User\\Desktop\\Audit.realm");
+
+            var realm = Realm.GetInstance(config);
+
+            // Update and persist objects with a thread-safe transaction
+            realm.Write(() => { realm.Add(new Speaker { Name = "Pavel" }); });
         }
     }
 }
