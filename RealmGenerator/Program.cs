4db435b1d02457e6a45792bfc822bcfa71fd7d00
Pavel <pavel.fedotovsky@outlook.com>
2018-03-09 00:57:37 +0300

    Added meetups

--- RealmGenerator/Program.cs
+++ RealmGenerator/Program.cs
@@ -26,42 +26,58 @@
 
             Mapper.Initialize(
                 cfg =>
-                {
-                    cfg.CreateMap<SpeakerEntity, Speaker>().AfterMap(
-                        (src, dest) =>
-                            {
-                                var speakerId = src.Id;
+                    {
+                        cfg.CreateMap<SpeakerEntity, Speaker>().AfterMap(
+                            (src, dest) =>
+                                {
+                                    var speakerId = src.Id;
 
-                                dest.AvatarSmall = AuditHelper.LoadImage("speakers", speakerId, "avatar.small.jpg");
-                                dest.Avatar = AuditHelper.LoadImage("speakers", speakerId, "avatar.jpg");
-                            });
-                    cfg.CreateMap<VenueEntity, Venue>();
-                    cfg.CreateMap<FriendEntity, Friend>().AfterMap(
-                        (src, dest) =>
-                            {
-                                var friendId = src.Id;
-                                
-                                dest.LogoSmall = AuditHelper.LoadImage("friends", friendId, "logo.small.png");
-                                dest.Logo = AuditHelper.LoadImage("friends", friendId, "logo.png");
-                            });
-                    cfg.CreateMap<CommunityEntity, Community>();
-                    cfg.CreateMap<TalkEntity, Talk>().AfterMap(
-                        (src, dest) =>
-                        {
-                            foreach (string speakerId in src.SpeakerIds)
-                            {
-                                var speaker = realm.Find<Speaker>(speakerId);
+                                    dest.AvatarSmall = AuditHelper.LoadImage("speakers", speakerId, "avatar.small.jpg");
+                                    dest.Avatar = AuditHelper.LoadImage("speakers", speakerId, "avatar.jpg");
+                                });
+                        cfg.CreateMap<VenueEntity, Venue>();
+                        cfg.CreateMap<FriendEntity, Friend>().AfterMap(
+                            (src, dest) =>
+                                {
+                                    var friendId = src.Id;
 
-                                dest.Speakers.Add(speaker);
-                            }
-                        });
-                });
+                                    dest.LogoSmall = AuditHelper.LoadImage("friends", friendId, "logo.small.png");
+                                    dest.Logo = AuditHelper.LoadImage("friends", friendId, "logo.png");
+                                });
+                        cfg.CreateMap<CommunityEntity, Community>();
+                        cfg.CreateMap<TalkEntity, Talk>().AfterMap(
+                            (src, dest) =>
+                                {
+                                    foreach (string speakerId in src.SpeakerIds)
+                                    {
+                                        var speaker = realm.Find<Speaker>(speakerId);
+
+                                        dest.Speakers.Add(speaker);
+                                    }
+                                });
+                        cfg.CreateMap<MeetupEntity, Meetup>().AfterMap(
+                            (src, dest) =>
+                                {
+                                    foreach (string talkId in src.TalkIds)
+                                    {
+                                        var talk = realm.Find<Talk>(talkId);
+                                        dest.Talks.Add(talk);
+                                    }
+
+                                    foreach (string friendId in src.FriendIds)
+                                    {
+                                        var friend = realm.Find<Friend>(friendId);
+                                        dest.Friends.Add(friend);
+                                    }
+                                });
+                    });
 
             realm.AddEntities<SpeakerEntity, Speaker>("speakers");
             realm.AddEntities<FriendEntity, Friend>("friends");
             realm.AddEntities<VenueEntity, Venue>("venues");
             realm.AddEntities<CommunityEntity, Community>("communities");
             realm.AddEntities<TalkEntity, Talk>("talks");
+            realm.AddEntities<MeetupEntity, Meetup>("meetups");
         }
 
         private static void CleanDirectory(string realmDirectoryPath)
